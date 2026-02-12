20e662a6b100fe28d90ed2fa35280d1e566c4629
Chiie <nguyenthimaichi248@gmail.com>
2026-02-12 14:31:09 +0700

    chinh sua loi khong tu dong drawer

--- app/src/main/java/com/example/btl/MainActivity.java
+++ app/src/main/java/com/example/btl/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.core.view.GravityCompat;
+
 import com.google.android.material.navigation.NavigationView;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -39,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
                 this, navController, appBarConfiguration);
 
         NavigationUI.setupWithNavController(navView, navController);
+
+        // đóng DRAWER
+        navView.setNavigationItemSelectedListener(item -> {
+
+            boolean handled = NavigationUI.onNavDestinationSelected(item, navController);
+
+            if (handled) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+
+            return handled;
+        });
     }
 
 
