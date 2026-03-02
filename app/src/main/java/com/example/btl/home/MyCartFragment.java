df5f7a03694624f922f97ced828de9da97105243
Chiie <nguyenthimaichi248@gmail.com>
2026-03-02 08:00:12 +0700

    gio hang done

---
+++ app/src/main/java/com/example/btl/home/MyCartFragment.java
@@ -0,0 +1,52 @@
+package com.example.btl.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.btl.R;
+import com.example.btl.adapters.CartAdapter;
+import com.example.btl.models.CartModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyCartFragment extends Fragment {
+
+    List<CartModel> list;
+    CartAdapter cartAdapter;
+    RecyclerView recyclerView;
+
+    public MyCartFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my_cart, container, false);
+
+        recyclerView = view.findViewById(R.id.cart_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        list = new ArrayList<>();
+
+        list.add(new CartModel(R.drawable.s1, "Order 1", "30", "4.3"));
+        list.add(new CartModel(R.drawable.s2, "Order 1", "$20", "4.6"));
+        list.add(new CartModel(R.drawable.fav1, "Order 1", "40", "4.8"));
+        list.add(new CartModel(R.drawable.s1, "Order 1", "$30", "4.3"));
+        list.add(new CartModel(R.drawable.s2, "Order 1", "$20", "4.9"));
+        list.add(new CartModel(R.drawable.fav1, "Order 1", "40", "5.0"));
+
+        cartAdapter = new CartAdapter(list);
+        recyclerView.setAdapter(cartAdapter);
+
+        return view;
+    }
+}
