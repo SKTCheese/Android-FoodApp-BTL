017ab36ee8f954286727d1befb6f3d7daa4f3c9f
Chiie <nguyenthimaichi248@gmail.com>
2026-03-16 08:37:23 +0700

    xoa bo regis, chi giu lai login

--- app/src/main/java/com/example/btl/WellcomeActivity.java
+++ app/src/main/java/com/example/btl/WellcomeActivity.java
@@ -6,28 +6,22 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class WellcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         setContentView(R.layout.activity_wellcome);
-        };
-
-    public void register(View view) {
-        startActivity(new Intent(WellcomeActivity.this, RegistrationActivity.class));
     }
 
     public void login(View view) {
-        startActivity(new Intent(WellcomeActivity.this, RegistrationActivity.class));
+        startActivity(new Intent(WellcomeActivity.this, LoginActivity.class));
     }
 }
